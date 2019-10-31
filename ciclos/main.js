0dccedb4c8206d17ebb95e2d0b1c2adba9b251b8
Diana Ibarra <diana.ibarra.reyes@gmail.com>
2019-10-30 21:49:42 -0600

    Carpera ciclos For

---
+++ ciclos/main.js
@@ -0,0 +1,31 @@
+/* for (let i = 0; i < 100; i = i + 1) {
+    console.log(i)
+} */
+
+/* for (let i = 5; i <= 1000; i = i + 5){
+    console.log(i)
+} */
+
+/* for(let i = 500; i >= 0; i = i -2){
+    console.log(i)
+} */
+
+/* for (let i = -763; i <= 15; i = i +3){
+    console.log (i)
+} */
+
+
+/* for (let i =1; i <=100; i = i + 1){
+    if ( i % 3 === 0 && i % 5 === 0){
+        console.log (i + " FizzBuzz")
+    } else if (i % 5 === 0){
+        console.log (i + " Buzz")
+    } else if (i  % 3 === 0){
+        console.log (i + " Fizz")
+    } else {
+        console.log (i)
+    }
+    
+} */
+
+//ejecutar enconsola las tablas del 1 al 10
